10eeb2fedafca3cac183ab48a5fb15771cc8218c
Emli O'Mago <emiliodesouzajunior@gmail.com>
2025-09-08 17:44:12 -0300

    🔧  Jogavel

--- Assets/Scripts/barraDeVida.cs
+++ Assets/Scripts/barraDeVida.cs
@@ -5,6 +5,12 @@ public class barraDeVida : MonoBehaviour
 {
     public Slider slider;
     
+    public void TakeDamage(float damage)
+    {
+        slider.value -= damage;
+        slider.value = Mathf.Min(slider.value, 0, slider.value);
+    }
+
     public void setMax(float max)
     {
         slider.maxValue = max;
